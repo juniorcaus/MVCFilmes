5d6e9045478c3f2594f932bceced0cdfbdf9c29a
juniorcaus <miltomjuniorcausgervasio@hotmail.com>
2023-05-02 21:14:30 -0300

    Adicionar arquivos de projeto.

---
+++ MVCFilmes/Controllers/FilmesAController.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MVCFilmes.Data;
+using MVCFilmes.Models;
+
+namespace MVCFilmes.Controllers
+{
+    public class FilmesAController : Controller
+    {
+        private readonly MVCFilmesContext _context;
+
+        public FilmesAController(MVCFilmesContext context)
+        {
+            _context = context;
+        }
+
+        // GET: FilmesA
+        public async Task<IActionResult> Index(string Texto, string Genero)
+        {
+
+           
+            //Query Genero
+            IQueryable<string> generos = from m in _context.Filmes
+                                         orderby m.Genero
+                                         select m.Genero;
+
+
+            //Query filme
+            var filmes = from m in _context.Filmes
+                         select m;
+
+            
+            //Filtro de Genero
+            if(!string.IsNullOrWhiteSpace(Genero))
+            {
+                filmes = filmes.Where(s => s.Genero == Genero);
+            }
+
+
+            //Filtro filme  
+            if(!String.IsNullOrWhiteSpace(Texto))
+            {
+                   filmes = filmes.Where( s => s.Titulo!.Contains(Texto));
+            }
+
+
+            // Essa aqui é a ViewModel
+            var filmeViewModel = new Models.FilmesViewModel
+            {
+                Filmes = await filmes.ToListAsync(),
+                Generos = new SelectList(await generos.Distinct().ToListAsync())
+            };
+
+            return View(filmeViewModel);
+            //return _context.Filmes != null ? View(await _context.Filmes.ToListAsync()) : Problem("Entity set 'MVCFilmesContext.Filmes'  is null.");
+        }
+
+        // GET: FilmesA/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Filmes == null)
+            {
+                return NotFound();
+            }
+
+            var filmes = await _context.Filmes
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (filmes == null)
+            {
+                return NotFound();
+            }
+
+            return View(filmes);
+        }
+
+        // GET: FilmesA/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: FilmesA/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Titulo,DataLancamento,Genero,Preco,Pontos")] Filmes filmes)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(filmes);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(filmes);
+        }
+
+        // GET: FilmesA/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Filmes == null)
+            {
+                return NotFound();
+            }
+
+            var filmes = await _context.Filmes.FindAsync(id);
+            if (filmes == null)
+            {
+                return NotFound();
+            }
+            return View(filmes);
+        }
+
+        // POST: FilmesA/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Titulo,DataLancamento,Genero,Preco, Pontos")] Filmes filmes)
+        {
+            if (id != filmes.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(filmes);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!FilmesExists(filmes.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(filmes);
+        }
+
+        // GET: FilmesA/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Filmes == null)
+            {
+                return NotFound();
+            }
+
+            var filmes = await _context.Filmes
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (filmes == null)
+            {
+                return NotFound();
+            }
+
+            return View(filmes);
+        }
+
+        // POST: FilmesA/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Filmes == null)
+            {
+                return Problem("Entity set 'MVCFilmesContext.Filmes'  is null.");
+            }
+            var filmes = await _context.Filmes.FindAsync(id);
+            if (filmes != null)
+            {
+                _context.Filmes.Remove(filmes);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool FilmesExists(int id)
+        {
+          return (_context.Filmes?.Any(e => e.ID == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ MVCFilmes/Models/Filmes.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MVCFilmes.Models
+{
+    public class Filmes
+    {
+            public int ID { get; set; }
+            public string Titulo { get; set; } = string.Empty;
+
+        [Display(Name = "Data de Lançamento")]
+
+           [DataType(DataType.Date)]
+            public DateTime DataLancamento { get; set; }
+            public string Genero { get; set;} = string.Empty;
+        [Display(Name = "Preço")]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Preco { get; set;}
+
+        public int? Pontos { get; set; }
+            
+            
+            
+
+    }
+}
